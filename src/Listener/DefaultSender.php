01aa7773f2a9676b7f3adfae4b09a38e6b087ca4
mark burdett <mfburdett@gmail.com>
2018-06-08 18:12:51 -0700

    the SMTP response code could be empty string

--- src/Listener/DefaultSender.php
+++ src/Listener/DefaultSender.php
@@ -190,7 +190,7 @@ class DefaultSender extends BaseListener {
     }
 
     // Register 5xx SMTP response code (permanent failure) as bounce.
-    if ($code{0} === '5') {
+    if (isset($code{0}) && $code{0} === '5') {
       return FALSE;
     }
 
